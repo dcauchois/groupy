5be936bf3c5112d255a3d49f5f47c37438cb3fd1
Denis <denis.cauchois@gmail.com>
2016-08-27 13:46:25 +0200

    Remplacement de Groupe + Territoire par structure

--- src/main/java/groupy/service/TerritoireService.java
+++
@@ -1,8 +0,0 @@
-package groupy.service;
-
-import groupy.entity.Territoire;
-
-public interface TerritoireService extends GenericService<Territoire> {
-	
-	Territoire retrieveTerritoireByLibelle(String libelle);
-}
